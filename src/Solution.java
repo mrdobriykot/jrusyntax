f555dd12e10182d1d3e3214eae10684b46148b55
mrdobriykot <evgkorin@yandex.ru>
2022-05-26 14:04:43 +0300

    mentors feedback

--- src/Solution.java
+++ src/Solution.java
@@ -12,13 +12,13 @@ public class Solution {
         boolean work = true;
         while (work) {
             Scanner scanner = new Scanner(System.in);
-            System.out.println("1 - зашифровать, 2 - расшифровать, 3 - буртфорс шифра. 4 - выход");
+            System.out.println("1 - зашифровать, 2 - расшифровать, 3 - брутфорс шифра. 4 - выход");
             System.out.print("Выберите действие: ");
             int choice = scanner.nextInt();
             switch (choice) {
                 case 1 -> encrypt();
                 case 2 -> decrypt();
-                case 3 -> bruteForce();
+                case 3 -> brute_force();
                 case 4 -> work = false;
             }
         }
@@ -33,7 +33,7 @@ public class Solution {
         String encryptedFilePath = scanner.nextLine();
         System.out.print("Введите шаг шифрования: ");
         int shiftNumber = scanner.nextInt();
-        de_en_crypt_data_from_file(sourceFilePath, encryptedFilePath, shiftNumber, true);
+        crypt_data_from_file(sourceFilePath, encryptedFilePath, shiftNumber, true);
     }
 
 
@@ -45,23 +45,23 @@ public class Solution {
         String encryptedFilePath = scanner.nextLine();
         System.out.print("Введите шаг расшифрования: ");
         int shiftNumber = scanner.nextInt();
-        de_en_crypt_data_from_file(sourceFilePath, encryptedFilePath, shiftNumber, false);
+        crypt_data_from_file(sourceFilePath, encryptedFilePath, shiftNumber, false);
     }
 
 
-    public static void bruteForce(){
+    public static void brute_force(){
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите путь до файла, который надо дешифровать: ");
         String sourceFilePath = scanner.nextLine();
         System.out.print("Введите путь до дешифрованного файла файла: ");
         String encryptedFilePath = scanner.nextLine();
         for (int shift = 0; shift <= alphabet.length(); shift++) {
-            de_en_crypt_data_from_file(sourceFilePath, encryptedFilePath, shift, false);
+            crypt_data_from_file(sourceFilePath, encryptedFilePath, shift, false);
         }
     }
 
 
-    public static void de_en_crypt_data_from_file(String planTextFile, String encryptedFile, int shift, boolean enc_flag) {
+    private static void crypt_data_from_file(String planTextFile, String encryptedFile, int shift, boolean enc_flag) {
         try(FileReader reader = new FileReader(planTextFile);
             FileWriter writer = new FileWriter(encryptedFile,true)
         ) {
@@ -76,7 +76,7 @@ public class Solution {
     }
 
 
-    public static char symbol_right_shift(char symbol, int shift, boolean enc_flag) {
+    private static char symbol_right_shift(char symbol, int shift, boolean enc_flag) {
         String reversedString = new StringBuffer(alphabet).reverse().toString();
         if (alphabet.indexOf(symbol) != -1 && enc_flag) {
             return alphabet.charAt((alphabet.indexOf(symbol) + shift) % alphabet.length());
